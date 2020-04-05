9cd3e8868e046b3d00e5b106eeb6de4d6e163a58
MikiKru <michal_kruczkowski@o2.pl>
2020-04-05 16:46:39 +0200

    python db v15

--- main.py
+++ main.py
@@ -9,26 +9,31 @@ class CLI:
             print("Witaj w aplikacji TASK MANAGER")
             decision = input("(L) - logowanie \n(R) - rejestracja \n(Q) - wyjście").upper()
             if(decision == "L"):
-                if(self.tmc.login(input("podaj email:"), input("podaj password"))):
-                    self.menu()
+                log = self.tmc.login(input("podaj email:"), input("podaj password"))    # zwraca bool -> decyzja, int -> user_id
+                if(log[0]):
+                    self.menu(log[1])
             elif(decision == "R"):
                 print("Jeszcze nie jest aktywna rejestrcja")
             else:
                 print("Wyjście")
                 break               # gdy program napotka break wychodzi z aktualnie wykonywanej pętli
-    def menu(self):
+    def menu(self, user_id):
         while(True):
             decision = input("(1) - dodaj zadanie \n(2) - wypisz zadania "
                              "\n(3) - usuń zadanie \n(4) - zmień hasło \n(Q) - cofnij").upper()
             if(decision == "1"):
-                pass
+                self.tmc.insertTaskByUser(
+                    input("podaj nazwę "),
+                    input("podaj opis "),
+                    input("podaj kategorię ('SQL', 'Git', 'Python', 'PythonLib', 'ML') "),
+                    user_id)
             elif(decision == "2"):
                 self.tmc.selectTasks()
             elif (decision == "3"):
                 self.tmc.selectTasks()
                 self.tmc.deleteTaskById(int(input("podaj id zadania")))
             elif (decision == "4"):
-                pass
+                self.tmc.updateUserPassword(user_id, input("podaj nowe hasło"))
             elif (decision == "Q"):
                 break
             else:
