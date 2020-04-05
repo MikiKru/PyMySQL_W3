1fad68709a5e2872f74a6f594bac979b674fe9d7
MikiKru <michal_kruczkowski@o2.pl>
2020-04-05 11:41:08 +0200

    python db v2

--- config/tm_connect.py
+++ config/tm_connect.py
@@ -1,17 +1,33 @@
 import pymysql
+# auto-komentowanie - CTRL + /
+class ConnectionConfig:
+    def connection(self, user="tm_user", passwd="qwe123"):
+        self.conn = pymysql.connect("localhost", user, passwd, "tm_db")
+        if(self.conn):
+            print("...połączono z bazą danych...")
+        else:
+            print("błąd połączenia")
+        return self.conn
+    def closeConnection(self):
+        self.conn.close()
+        print("...połączenie zakmniete...")
 
-def connection(user = "tm_user", passwd= "qwe123"):
-    global conn     # zakres widoczności obiektu conn dotyczy całego skryptu
-    conn = pymysql.connect("localhost", user, passwd, "tm_db")
-    if(conn):
-        print("...połączono z bazą danych...")
-    else:
-        print("błąd połączenia")
-    return conn
+cc = ConnectionConfig()     # wywolanie kontruktora klasy
+cc.connection()
+cc.closeConnection()
 
-def closeConnection():
-    conn.close()
-    print("...połączenie zakmniete...")
-
-connection()
-closeConnection()
+# def connection(user = "tm_user", passwd= "qwe123"):
+#     global conn     # zakres widoczności obiektu conn dotyczy całego skryptu
+#     conn = pymysql.connect("localhost", user, passwd, "tm_db")
+#     if(conn):
+#         print("...połączono z bazą danych...")
+#     else:
+#         print("błąd połączenia")
+#     return conn
+#
+# def closeConnection():
+#     conn.close()
+#     print("...połączenie zakmniete...")
+#
+# connection()
+# closeConnection()
--- sql/data.sql
+++ sql/data.sql
@@ -4,4 +4,32 @@ create database tm_db;
 create user 'tm_user'@'localhost' identified by 'qwe123';
 -- przypisanie uprawnień dla użytkownika dla bazy tm_db
 -- SELECT, INSERT, DELETE, UPDATE -> DML
-grant SELECT, INSERT, DELETE, UPDATE on tm_db.* to 'tm_user'@'localhost'; 
+grant SELECT, INSERT, DELETE, UPDATE on tm_db.* to 'tm_user'@'localhost';
+
+-- struktura tabel
+use tm_db;
+create table user(
+	user_id int primary key auto_increment,
+    name varchar(255) not null,
+    lastname varchar(255) not null,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    registraton_date datetime default now(),
+	enable bool default 1
+);
+create table task(
+	task_id int primary key auto_increment,
+    task_name varchar(255) not null,
+    task_description text not null,
+    task_category enum('SQL', 'Git', 'Python', 'PythonLib', 'ML') not null,
+    user_id int,
+    foreign key (user_id) references user (user_id)
+);
+create table subtask(
+	subtask_id int primary key auto_increment,
+    subtask_name varchar(255) not null,
+    subtask_date_start date,
+    subtask_date_stop date,
+    task_id int,
+    foreign key (task_id) references task (task_id)
+);
